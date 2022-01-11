0e51038dbc896e429e75147abbc71653506f53b4
saint369 <saint369@gmail.com>
2022-01-12 01:11:38 +0800

    Update jd_travel.js

--- jd_travel.js
+++ jd_travel.js
@@ -4,7 +4,7 @@ if (process.env.ZNS != 'true') {
 }
 /*
 被内鬼偷给柠檬了,大家一起玩吧
-33 0,6-23/2 * * * jd_travel.js
+21 0,6-23/3 * * * jd_travel.js
 */
 const $ = new Env('炸年兽');
 const notify = $.isNode() ? require('./sendNotify') : '';
